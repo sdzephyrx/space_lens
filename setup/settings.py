c77d49c503fc77f226adf8755a56725225eb20cf
Bruno Lima <sdzephyrx@gmail.com>
2023-10-02 10:20:45 -0300

    configuracao de formularios e autenticacao de usuarios

--- setup/settings.py
+++ setup/settings.py
@@ -136,3 +136,10 @@ MEDIA_URL = "/media/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+    messages.SUCCESS: 'success',
+}
