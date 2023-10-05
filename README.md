074d72045fe75519facc1263164a00b905bbc045
Bruno Lima <sdzephyrx@gmail.com>
2023-10-05 09:52:30 -0300

    adicao e configuracao do amazon s3 para persistencia de dados

--- README.md
+++ README.md
@@ -7,12 +7,26 @@ Front-end disponibilizado por Alura cursos.
 virtual para execução do projeto.
 Comando para rodar no ambiente virtual para instalação dos pacotes:
   - pip install -r requirements.txt
-  - echo "SECRET_KEY=`python generate_secret_key.py`" > .env
+  - echo "SECRET_KEY=`python scripts/secret_key_generator.py`" > .env
   - python manage.py migrate ( para importar o schema do banco )
   - python manage.py createsuperuser ( para criar o usuario admin do banco )
 
 Caso ocorra de não carregar as imagens, rodar o seguinte comando:
   - python manage.py collectstatic (para import de assets)
+
+Para o uso do S3 da amazon, é necessário uma conta, um bucket s3 e chaves de 
+acesso válidas. Essas infos devem ser salvos no arquivo .env com as seguintes
+variaveis:
+  -AWS_ACCESS_KEY_ID 
+  -AWS_SECRET_ACCESS_KEY
+  -AWS_STORAGE_BUCKET_NAME
+No arquivo setup/settings.py, é necessário a configuração das seguintes variaveis para o uso
+do s3:
+  -AWS_DEFAULT_ACL
+  -AWS_S3_OBJECT_PARAMETERS
+  -AWS_LOCATION
+  -AWS_QUERYSTRING_AUTH
+  -AWS_HEADERS
 
 Apos o clone do projeto, use o seguinte comando para iniciar o servidor:
   - python manage.py runserver (para rodar o servidor)
--- setup/settings.py
+++ setup/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
 
     'apps.galeria.apps.GaleriaConfig',
     'apps.usuarios.apps.UsuariosConfig',
+
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -117,11 +119,30 @@ USE_I18N = True
 
 USE_TZ = True
 
+#AWS
+
+AWS_ACCESS_KEY_ID = str(os.getenv('AWS_ACCESS_KEY_ID'))
+AWS_SECRET_ACCESS_KEY = str(os.getenv('AWS_SECRET_ACCESS_KEY'))
+AWS_STORAGE_BUCKET_NAME = str(os.getenv('AWS_STORAGE_BUCKET_NAME'))
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_DEFAULT_ACL = 'public-read'
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400'
+}
+AWS_LOCATION = 'static'
+AWS_QUERYSTRING_AUTH = False
+AWS_HEADERS = {
+    'Access-Control-Allow-Origin': '*'
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'setup/static')
@@ -131,7 +152,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-MEDIA_URL = "/media/"
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -144,3 +165,4 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
     messages.SUCCESS: 'success',
 }
+
